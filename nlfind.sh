e43fb55e03834439b0667dfeb82fa6be31c85a90
Landon Curt Noll <github-mail@asthe.com>
1997-08-25 03:40:41 +0000

    Initial revision

---
+++ nlfind.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# nlfind - find files with a newline in their name
+#
+# usage:
+#
+#	nlfind dir find_args...
+
+# parse args
+#
+if [ $# -lt 2 ]; then
+    echo "usage: $0 dir find_args..." 1>&2
+    exit 1
+fi
+dir="$1"
+shift
+
+# find them
+#
+/sbin/find "$dir" \( -name '*
+*' \) -a \( $@ \)
