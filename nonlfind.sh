cd49759668be83be7c55a4ea89bbec0c1a9a2e61
Landon Curt Noll <github-mail@asthe.com>
1997-08-25 03:31:30 +0000

    Initial revision

---
+++ nonlfind.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# nonlfind - find files without a newline in their name
+#
+# usage:
+#
+	nonlfind dir find_args...
+
+# parse args
+#
+if [ $# -lt 2 ]; then
+    echo "usage: $o dir find_args..." 1>&2
+    exit 1
+fi
+dir="$1"
+shift
+
+# find them
+#
+/sbin/find "$dir" ! -name '*
+*' $@
